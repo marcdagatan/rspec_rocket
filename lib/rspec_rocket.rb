a38cacbb737737f080599fe9bb8c8168c2e5262e
Marc Lawrence U. Dagatan <marc.dagatan@gmail.com>
2024-12-14 15:03:45 +0800

    Initial commit

---
+++ lib/rspec_rocket.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rspec_rocket/version"
+
+module RspecRocket
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rspec_rocket/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RspecRocket
+  VERSION = "0.1.0"
+end
